bdc01b22faa6a1aafb9df5e2fbbc44c1de4377ce
xzll <807738968@qq.com>
2018-08-07 15:35:17 +0800

    netty的小demo-时间客户端

---
+++ nettydemo/src/main/java/nettybeginner/TimeClient.java
@@ -0,0 +1,47 @@
+package nettybeginner;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * Created by moon on 2017/4/6.
+ */
+public class TimeClient {
+    public static void main(String[] args) {
+//        String host = args[0];
+//        int port = Integer.parseInt(args[1]);
+        String host = "127.0.0.1";
+        int port = Integer.parseInt("8080");
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            Bootstrap b = new Bootstrap(); // (1)用于非服务器通道
+            b.group(workerGroup); // (2)该group同时用作一个老板组和一个工作人员组
+            b.channel(NioSocketChannel.class); // (3)指定NioSocketChannel来实例化客户端通道
+            b.option(ChannelOption.SO_KEEPALIVE, true);
+            b.handler(new ChannelInitializer<SocketChannel>() {// (4)不使用childHandler 因为客户端SocketChannel没有父级
+                @Override
+                public void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new TimeClientHandler());
+                }
+            });
+
+            // Start the client.
+            ChannelFuture f = b.connect(host, port).sync(); // (5)用connect 不用bind
+
+            // Wait until the connection is closed.
+            f.channel().closeFuture().sync();
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+        }
+
+
+    }
+}
---
+++ nettydemo/src/main/java/nettybeginner/TimeClientHandler.java
@@ -0,0 +1,32 @@
+package nettybeginner;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.concurrent.EventExecutorGroup;
+
+import java.util.Date;
+
+public class TimeClientHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+//        super.channelRead(ctx, msg);
+        ByteBuf m = (ByteBuf) msg; // (1)
+        try {
+            long currentTimeMills = (m.readUnsignedInt() - 2208988800L) * 1000L;//从服务端接收一个32位整数，将其翻译成可读的形式
+            System.out.println(new Date(currentTimeMills));
+            ctx.close();
+        } finally {
+            m.release();
+        }
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+//        super.exceptionCaught(ctx, cause);
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
